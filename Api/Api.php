2b0c5a84eb2b61b538ccaba520b98dba43bad022
Amine <kurosoul.ae@gmail.com>
2017-03-29 12:22:07 +0100

    Initial commit

---
+++ Api/Api.php
@@ -0,0 +1,79 @@
+<?php
+namespace Plugins\Api;
+use Exception;
+use Core\Response;
+
+class Api
+{
+
+	protected $app;
+	protected $config;
+	
+	function __construct($app)
+	{
+		$this->app = $app;
+		// load plugin configuration.
+		$this->config = get_plugin_config($this);
+
+		// check if the prefix is defined.
+		if ( ! array_get(get_object_vars($this->config), 'url') ) {
+			throw new Exception("URL is not defined.");
+		}
+
+		$this->app->event->on('before.routes', [$this, 'setup']);
+	}
+
+	/**
+	 * Setup routes.
+	 * @param  array &$routes
+	 * @return
+	 */
+	public function setup(&$routes)
+	{
+		// hook all api urls
+		$routes[$this->config->url . '(.*)'] = [$this, 'dispatcher'];
+
+		// by pass the index page.
+		$routes[''] = function () { return ''; };
+
+		// 404 all other pages.
+		$routes['(.*)'] = [$this, 'disableNoneApiCalls'];
+	}
+
+	/**
+	 * Short circuit none api calls
+	 */
+	public function disableNoneApiCalls()
+	{
+		$response = new Response;
+		exit($response->redirect('/'));
+	}
+
+	/**
+	 * Dispatching the api call
+	 * @return string
+	 */
+	public function dispatcher()
+	{
+		$this->app->event->on('before.render', [$this, 'response']);
+		return preg_replace('~^' . preg_quote($this->config->url, '/') . '~', '', $this->app->request->uri());
+	}
+
+	/**
+	 * Api response as json.
+	 * @return string
+	 */
+	function response() {
+		$data = $this->app->page->header();
+		$data['content'] = $this->app->page->content();
+
+		if (array_get($data, 'layout') == '404')
+		{
+			$this->notFound();
+		}
+		$response = new Response;
+		$response->header('Content-Type', 'application/json')
+				->content(json_encode($data));
+		exit($response);
+	}
+}
---
+++ Api/Readme.md
@@ -0,0 +1,11 @@
+# Apier
+
+apier is a plugin for flaty, that turns the response to json, so it can be used as an api.
+
+## How to install:
+
+Just put the folder inside the plugins directory on [flaty](https://github.com/oversoul/flaty)
+
+## How to use:
+
+Just access flaty using same urls, but preceeding with the prefix specified in the `config.json` of the plugin.
